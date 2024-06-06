36b00e4f085048346618485bed620dafe1bb1693
hungry <bob@gim-wontaes-MacBook-Pro.local>
2024-06-06 15:32:12 +0900

    main으로 바로 가기 버튼 추가 및 exceptionCode 업데이트

--- src/main/kotlin/com/sinsa/web/common/ExceptionCode.kt
+++ src/main/kotlin/com/sinsa/web/common/ExceptionCode.kt
@@ -8,6 +8,7 @@ enum class ExceptionCode(
     PRODUCT_UPDATE_FAIL("상품 정보 업데이트에 실패 하였습니다."),
     PRODUCT_SAVE_FAIL("상품 정보 저장에 실패 하였습니다."),
     PRODUCT_CATEGORY_BRAND_NOT_ENOUGH("해당 브랜드로 등록된 카테고리 상품이 충분치 않아 삭제할 수 없습니다."),
+    PRODUCT_CATEGORY_BRAND_NOT_ENOUGH_UPDATE("해당 브랜드로 등록된 카테고리 상품이 충분치 않아 수정할 수 없습니다."),
 
     //BRAND
     BRAND_NOT_EXIST("브랜드가 존재하지 않습니다.")
